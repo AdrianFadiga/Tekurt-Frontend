79c155bbc49fc5e06de23c91a2afd1ac8dbfe893
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-24 19:28:46 -0300

    feat: componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+interface Attributes {
+  holder: string
+  innerRef: React.RefObject<HTMLInputElement>
+  type: string
+  errorMessage: string
+}
+
+const Input: React.FC<Attributes> = ({ holder, innerRef, type, errorMessage }) => {
+  const [invalidField, setInvalidField] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
+  const verifyLengthInput = () => {    
+    const value = innerRef.current?.value;
+    
+    setMessageError(false);
+    setInvalidField(!value?.length);
+  };
+
+  const setFieldStatus = () => {    
+    if (invalidField) {
+      setMessageError(true);
+      setInvalidField(false);
+    } else {
+      setMessageError(false);
+      setInvalidField(false);      
+    }
+  };
+
+  return (
+    <div>
+      <input
+        required
+        type={ type }
+        ref={ innerRef }
+        placeholder={ holder }
+        onFocus={ setFieldStatus }
+        onBlur={ verifyLengthInput }
+        onInvalid={ () => setInvalidField(true)}
+      />
+
+      { invalidField && <span>campo errado</span>}
+      { messageError && <span>{ errorMessage }</span> }
+    </div>
+    
+  );
+};
+
+export default Input;
