2c9e4bb113c78fc5c9f894f49edcc2329a680298
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-28 12:39:13 -0300

    refact: Formulario de login agora em componente

---
+++ src/components/FormLogin/index.tsx
@@ -0,0 +1,79 @@
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IResponseAPI } from '../../interfaces/IResponseAPI';
+import { createOptionsRequest } from '../../services/createOptionsRequest';
+import { requestAPI } from '../../services/requestAPI';
+import { setToken } from '../../services/setTokenLocalStorage';
+import Input from '../Input';
+
+function FormLogin() {
+  const userInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+  const [invalidUser, setInvalidUser] = useState(false);
+  const navigate = useNavigate();
+
+  const verifyFields = () => {
+    const user = userInputRef.current?.value;
+    const password = passwordInputRef.current?.value;
+    
+    return !user?.length || !password?.length;
+  };
+
+  const sucessRequest = (response: IResponseAPI) => {
+    const { token } = response.data;
+    setToken(token);
+    navigate('/feed');
+  };
+
+  const failRequest = (response: IResponseAPI) => {
+    if (response.status === 404) setInvalidUser(true);
+    navigate('/deu-ruim');
+  };
+
+  const sigIn = async () => {
+    const isInvalidFields = verifyFields();
+
+    if(isInvalidFields) setInvalidUser(true);
+    else {
+      const user = userInputRef.current?.value;
+      const password = passwordInputRef.current?.value;
+
+      const options = createOptionsRequest('POST', { user, password }, 'login');
+
+      const response = await requestAPI(options);
+      if (response.error) failRequest(response);
+      else sucessRequest(response);
+    }
+  };
+
+  return (
+    <form onSubmit={ (event) => event.preventDefault() }>
+
+      <Input
+        holder='Email ou usuário'
+        innerRef={ userInputRef }
+        type="text"
+        errorMessage="Insira o seu email ou nome de usuario"
+      />
+
+      <br></br>
+
+      <Input
+        holder='Senha'
+        innerRef={ passwordInputRef }
+        type="password"
+        errorMessage="Insira a sua senha"
+      />
+
+      <br></br>
+
+      <button type="submit" onClick={ sigIn }>
+          Entrar
+      </button>
+
+      { invalidUser && <span>Usuário ou senha incorreto</span> }
+    </form>
+  );
+}
+
+export default FormLogin;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>    
