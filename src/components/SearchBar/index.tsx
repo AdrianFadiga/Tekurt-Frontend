0f8353da6a536992c9f8467b2e3725490cdac8ba
AdrianFadiga <adrianmfadiga@gmail.com>
2022-09-09 23:47:39 -0300

    feat: funcionalidade de buscar usuários conforme o filtro informado

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import { IUser } from '../../interfaces/IUser';
+import { createOptionsRequest } from '../../services/createOptionsRequest';
+import { requestAPI } from '../../services/requestAPI';
 import SearchScreen from '../SearchScreen';
 import { SearchStyle } from './style';
 
 function SearchBar() {
   const [searchValue, setSearchValue] = useState('');
   const [openModal, setOpenModal] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState<IUser[]>([]);
+
+  const getAllUsers = async () => {
+    const fetchRoute = searchValue ? `users/?filter=${searchValue}` : 'users';
+    const token = localStorage.getItem('authTekurt');
+    const options = createOptionsRequest('GET', {}, fetchRoute, {authorization: `Bearer ${token}`});
+    const response = await requestAPI<IUser[]>(options);
+    setFilteredUsers(response.data);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAllUsers();
+    }, 2000);
+  }, [searchValue]);
+
+  // useEffect(() => {
+  //   const foundUsers = allUsers.filter((user) => [user.username.toLowerCase(), user.firstName.toLowerCase(), user.lastName.toLowerCase()]
+  //     .some((u) => u.includes(searchValue.toLowerCase())));
+  //   setFilteredUsers(foundUsers);
+  // }, [searchValue]);
 
   const setModal = () => {
     setOpenModal(!openModal);
@@ -30,7 +54,9 @@ function SearchBar() {
       </div>
       
       { openModal && (
-        <SearchScreen setModal={ setModal }/>
+        <SearchScreen 
+          setModal={ setModal }
+          filteredUsers={filteredUsers}/>
       ) }
     </SearchStyle>
   );
