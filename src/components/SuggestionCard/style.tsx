400b6eee07ef5ce95d538de9192bed07aeb8a3ff
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-10-05 20:39:33 -0300

    refact/style: card de sugestão com a cor correta no tema dark

--- src/components/SuggestionCard/style.tsx
+++ src/components/SuggestionCard/style.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SuggestionCardStyle = styled.section`
-  border-radius: 5px;
+  border-radius: 10px;
   box-shadow: -1px 0px 11px -1px #0000003d;
   width: 220px;
   height: 40vh;
@@ -9,6 +9,7 @@ export const SuggestionCardStyle = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.background};
 
   .image-profile {
     height: 60%;
@@ -23,6 +24,7 @@ export const SuggestionCardStyle = styled.section`
   }
 
   .section-user {
+    background-color: transparent;
     padding: 8px;
     height: 15%;
   }
@@ -31,13 +33,16 @@ export const SuggestionCardStyle = styled.section`
     color: ${({ theme }) => theme.colors.secondary};
     font-size: 1.4em;
     font-weight: 600;
+    background-color: transparent;
   }
 
   .fullname {
+    background-color: transparent;
     color: ${({ theme }) => theme.colors.placeholder};
   }
 
   .section-invite {
+    background-color: transparent;
     height: 25%;
     display: flex;
     flex-direction: column;
